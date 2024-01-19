e848d6a56ffc1c8630f88677df0f0a602289ad2a
technicaljicama <gameovergurke@gmail.com>
2024-01-19 20:56:21 +0100

    AL stub

---
+++ minijvm/java/src/main/java/org/lwjgl/openal/AL10.java
@@ -0,0 +1,29 @@
+package org.lwjgl.openal;
+
+public class AL10 {
+    public void alBufferData(int a1, int a2, java.nio.ByteBuffer a3, int a4) {
+    }
+    
+    public void alDeleteBuffers(java.nio.IntBuffer a1) {
+    }
+    
+    public void alGenBuffers(java.nio.IntBuffer a1) {
+    }
+    
+    public void alGenSources(java.nio.IntBuffer a1) {
+    }
+    
+    public int alGetError() {
+        return 0;
+    }
+    
+    public void alListener(int a1, java.nio.FloatBuffer a2) {
+    }
+    
+    public void alListenerf(int a1, float a2) {
+    }
+    
+    public void alSourcef(int a1, int a2, float a3) {
+    }
+}
+
