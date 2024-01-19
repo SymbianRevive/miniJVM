d7b3e1c26007874569663f63c3368338b5d0ba43
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2024-01-19 11:38:59 +0100

    Indev works :D

--- minijvm/java/src/main/java/org/lwjgl/opengl/ContextCapabilities.java
+++ minijvm/java/src/main/java/org/lwjgl/opengl/ContextCapabilities.java
@@ -4,6 +4,10 @@ public class ContextCapabilities  {
     public final static boolean GL_NV_fog_distance = false;
     public final static boolean GL_ARB_vertex_buffer_object = false;
     public final static boolean GL_ARB_occlusion_query = false;
+    public final static boolean GL_ARB_compatibility = false;
+    public final static boolean OpenGL30 = false;
+    public final static boolean OpenGL31 = false;
+    public final static boolean OpenGL32 = false;
 
     ContextCapabilities() {}
 }
