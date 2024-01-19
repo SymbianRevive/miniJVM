28edb67be808be4abdf84dbeea32de25e067d03f
technicaljicama <gameovergurke@gmail.com>
2024-01-19 20:56:21 +0100

    AL stub

---
+++ minijvm/java/src/main/java/org/lwjgl/openal/AL.java
@@ -0,0 +1,13 @@
+package org.lwjgl.openal;
+
+public class AL {
+    public void create() {
+    }
+    
+    public void destroy() {
+    }
+    
+    public boolean isCreated () {
+        return true;
+    }
+}
